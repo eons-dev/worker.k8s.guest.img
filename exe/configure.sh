c1d6b5ba81b029c2c9bf2f0812ced96cc400441c
eons <eons@eons.dev>
2023-04-24 02:59:11 -0700

    add shebangs and such

--- exe/configure.sh
+++ exe/configure.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+mkdir -p /etc/default
+
 echo "KUBELET_EXTRA_ARGS=\"\
 --node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')\" \
 " > /etc/default/kubelet
--- exe/setup_storage.sh
+++ exe/setup_storage.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 persist="/mnt/persist/$HOSTNAME"
 
 paths=(

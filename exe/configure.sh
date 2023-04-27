9ea6fbc0829ed6d1d4f42260b2f72e9d4cde86be
gilleygoat <rob.n.gilley@gmail.com>
2023-04-27 16:05:30 -0700

    update launch to include containerd and kubelet

--- exe/configure.sh
+++ exe/configure.sh
@@ -5,3 +5,5 @@ mkdir -p /etc/default
 echo "KUBELET_EXTRA_ARGS=\"\
 --node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')\" \
 " > /etc/default/kubelet
+
+echo "127.0.1.1 $HOSTNAME" >> /etc/hosts

d2fe3dbfb743484bf8ed5f26b6e68263f3c8db5d
eons <eons@eons.dev>
2023-05-06 20:20:13 -0700

    fix typo
    Co-authored-by: robert gilley <gilleygoat@users.noreply.github.com>

--- exe/mkkubelet.sh
+++ exe/mkkubelet.sh
@@ -2,5 +2,5 @@
 
 echo "kubelet \
 --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \
---node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')\" \
+--node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}') \
 " > /launch.d/60_kubelet

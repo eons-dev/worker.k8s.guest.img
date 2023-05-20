bb50984534cfcd99275f2c2aadb2ad9dfe063c70
eons <eons@eons.dev>
2023-05-20 13:06:53 -0700

    correct broken ln

--- exe/setup_storage.sh
+++ exe/setup_storage.sh
@@ -5,10 +5,11 @@ persist="/mnt/persist/$HOSTNAME"
 paths=(
 	/etc/kubernetes
 	/etc/tinc
-	/var/lib/kubelet
 )
 
 for path in ${paths[@]}; do
-	ln -sv $persist/$path $path
+	ln -sv $persist$path $path
 done
 
+# using the loop duplicates the last path???
+ln -sv $persist/var/lib/kubelet /var/lib/kubelet

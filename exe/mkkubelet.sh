f96dad2c58b1d0797a9d5a6e2352b6d27e740d5a
eons <eons@eons.dev>
2023-05-20 13:35:14 -0700

    set cgroup-driver for kubelet

--- exe/mkkubelet.sh
+++ exe/mkkubelet.sh
@@ -4,5 +4,6 @@ echo "kubelet \
 --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \
 --kubeconfig=/etc/kubernetes/kubelet.conf \
 --config=/var/lib/kubelet/config.yaml \
+--cgroup-driver=cgroupfs \
 --node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}') \
 " > /launch.d/60_kubelet

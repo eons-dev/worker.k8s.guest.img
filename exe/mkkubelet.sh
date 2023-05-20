0b999a925a0c99fa852e2ff09a9a2bffbd4c7bd7
eons <eons@eons.dev>
2023-05-20 12:28:35 -0700

    specify kubeconfig in kubelet service
    Co-authored-by: robert gilley <gilleygoat@users.noreply.github.com>

--- exe/mkkubelet.sh
+++ exe/mkkubelet.sh
@@ -2,6 +2,7 @@
 
 echo "kubelet \
 --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \
+--kubeconfig=/etc/kubernetes/kubelet.conf \
 --config=/var/lib/kubelet/config.yaml \
 --node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}') \
 " > /launch.d/60_kubelet

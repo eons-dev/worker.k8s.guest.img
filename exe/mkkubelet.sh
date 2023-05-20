d2baf37408587eee2b1fff25ea5a4d95f325a4e5
eons <eons@eons.dev>
2023-05-20 15:25:39 -0700

    Allow xattr on /var/lib

--- exe/mkkubelet.sh
+++ exe/mkkubelet.sh
@@ -7,3 +7,50 @@ echo "kubelet \
 --cgroup-driver=cgroupfs \
 --node-ip=$(ip addr show internode | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}') \
 " > /launch.d/60_kubelet
+
+mkdir -p /var/lib/kubelet
+cat << EOF > /var/lib/kubelet/config.yaml
+apiVersion: kubelet.config.k8s.io/v1beta1
+authentication:
+  anonymous:
+    enabled: false
+  webhook:
+    cacheTTL: 0s
+    enabled: true
+  x509:
+    clientCAFile: /etc/kubernetes/pki/ca.crt
+authorization:
+  mode: Webhook
+  webhook:
+    cacheAuthorizedTTL: 0s
+    cacheUnauthorizedTTL: 0s
+cgroupDriver: cgroupfs
+clusterDNS:
+- 10.111.2.10
+clusterDomain: cluster.local
+cpuManagerReconcilePeriod: 0s
+evictionPressureTransitionPeriod: 0s
+fileCheckFrequency: 0s
+healthzBindAddress: 127.0.0.1
+healthzPort: 10248
+httpCheckFrequency: 0s
+imageMinimumGCAge: 0s
+kind: KubeletConfiguration
+logging:
+  flushFrequency: 0
+  options:
+    json:
+      infoBufferSize: "0"
+  verbosity: 0
+memorySwap: {}
+nodeStatusReportFrequency: 0s
+nodeStatusUpdateFrequency: 0s
+rotateCertificates: true
+runtimeRequestTimeout: 0s
+shutdownGracePeriod: 0s
+shutdownGracePeriodCriticalPods: 0s
+staticPodPath: /etc/kubernetes/manifests
+streamingConnectionIdleTimeout: 0s
+syncFrequency: 0s
+volumeStatsAggPeriod: 0s
+EOF
--- exe/setup_storage.sh
+++ exe/setup_storage.sh
@@ -11,8 +11,9 @@ for path in ${paths[@]}; do
 	ln -sv $persist$path $path
 done
 
+# This was moved to mkkubelet.sh
 # Specifying the $path after var/lib somehow duplicates it???
-for path in $(ls $persist/var/lib/); do
-	rm -rfv /var/lib/$path
-	ln -sv $persist/var/lib/$path /var/lib/
-done
+# for path in $(ls $persist/var/lib/); do
+# 	rm -rfv /var/lib/$path
+# 	ln -sv $persist/var/lib/$path /var/lib/
+# done

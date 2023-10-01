ad83c3ba0971ccceb6f00b4a76e3b7ad58b91cb8
eons <eons@eons.dev>
2023-10-01 16:00:57 -0700

    prepare to use syncthing
    Co-authored-by: robert gilley <gilleygoat@users.noreply.github.com>

--- exe/setup_storage.sh
+++ exe/setup_storage.sh
@@ -17,11 +17,3 @@ if [ -d /var/lib/kubelet/pki/ ] && [ ! -z "$(ls /var/lib/kubelet/pki)" ]; then
 	rm -fv /var/lib/kubelet/pki/kubelet-client-current.pem
 	ln -sv /var/lib/kubelet/pki/$(ls /var/lib/kubelet/pki/ | tail -3 | head -1) /var/lib/kubelet/pki/kubelet-client-current.pem
 fi
-
-
-# This was moved to mkkubelet.sh
-# Specifying the $path after var/lib somehow duplicates it???
-# for path in $(ls $persist/var/lib/); do
-# 	rm -rfv /var/lib/$path
-# 	ln -sv $persist/var/lib/$path /var/lib/
-# done
